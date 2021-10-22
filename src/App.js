95187b453634657286e02ae970ba4c93053e94f2
Dmytro Salii <dmitriysalii@gmail.com>
2021-10-22 10:28:17 +0300

    done

--- src/App.js
+++ src/App.js
@@ -17,7 +17,20 @@ function App() {
     setState(prevState => ({ ...prevState, [value]: prevState[value] + 1 }));
   };
 
-const values = Object.keys(state);  
+  const  countTotalFeedBack = () => {
+    const { good, neutral, bad } = state;
+    return good + neutral + bad;
+  };
+
+ const countPositiveFeedBack = () => {
+    return Math.round((state.good / countTotalFeedBack()) * 100);
+  };
+
+
+  const values = Object.keys(state);
+  const totalLikes = countTotalFeedBack();
+  const { good, neutral, bad } = state;
+  
     return (
       <div className="App">
         <Section title="Please, Leave your feedback">
@@ -26,74 +39,17 @@ const values = Object.keys(state);
             onLeaveFeedback={onLeaveFeedback}
           />
         </Section>
-        {/* {totalLikes === 0 ? (
-          <Notification message="No feedback given" />
-        ) : (
+        {totalLikes === 0 ? (
+          <Notification message="No feedback given" />) : (
           <Statistics
             good={good}
             neutral={neutral}
             bad={bad}
             total={totalLikes}
-            positiveFeedBack={positiveFeedBack}
+            positiveFeedBack={countPositiveFeedBack()}
           ></Statistics>
-        )} */}
+        )}
       </div>
     );
-
-
 }
-
-// class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   onLeaveFeedback = value => {
-//     this.setState(prevState => {
-//       return {
-//         [value]: prevState[value] + 1,
-//       };
-//     });
-//   };
-
-//   countTotalFeedBack = () => {
-//     const { good, neutral, bad } = this.state;
-//     return good + neutral + bad;
-//   };
-
-//   countPositiveFeedBack = () => {
-//     return Math.round((this.state.good / this.countTotalFeedBack()) * 100);
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const totalLikes = this.countTotalFeedBack();
-//     const positiveFeedBack = this.countPositiveFeedBack();
-//     const values = Object.keys(this.state);
-//     return (
-//       <div className="App">
-//         <Section title="Please, Leave your feedback">
-//           <FeedBackOption
-//             options={values}
-//             onLeaveFeedback={this.onLeaveFeedback}
-//           />
-//         </Section>
-//         {totalLikes === 0 ? (
-//           <Notification message="No feedback given" />
-//         ) : (
-//           <Statistics
-//             good={good}
-//             neutral={neutral}
-//             bad={bad}
-//             total={totalLikes}
-//             positiveFeedBack={positiveFeedBack}
-//           ></Statistics>
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
 export default App;
